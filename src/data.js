14dadc27ba2b82e5333e113637c5af27a6700cfa
Jesse Krensel <jdkrensel@gmail.com>
2023-02-17 23:41:30 -0500

    add image property

--- src/data.js
+++ src/data.js
@@ -6,6 +6,7 @@ export const objects = [
       "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
     distance: "384,400 KM",
     time: "3 DAYS",
+    image: "assets/images/destination/image-moon.webp",
   },
   {
     id: 1,
@@ -14,6 +15,7 @@ export const objects = [
       "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
     distance: "225 MIL. km",
     time: "9 months",
+    image: "assets/images/destination/image-mars.webp",
   },
   {
     id: 2,
@@ -22,6 +24,7 @@ export const objects = [
       "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
     distance: "628 MIL. km",
     time: "3 years",
+    image: "assets/images/destination/image-europa.webp",
   },
   {
     id: 3,
@@ -30,6 +33,7 @@ export const objects = [
       "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
     distance: "1.6 BIL. km",
     time: "7 years",
+    image: "assets/images/destination/image-titan.webp",
   },
 ];
 export const [moon, mars, europa, titan] = objects;
