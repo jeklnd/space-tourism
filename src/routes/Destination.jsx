d2646889c17c3bbc72e18be714c20483182fb467
Jesse Krensel <jdkrensel@gmail.com>
2023-02-17 21:13:28 -0500

    add dynamic rendering based on routes, add styling

--- src/routes/Destination.jsx
+++ src/routes/Destination.jsx
@@ -4,16 +4,28 @@ import styles from "./Destination.module.css";
 import Nav from "components/Nav";
 import Subnav from "components/Subnav";
 import moon from "assets/images/destination/image-moon.webp";
+import Content from "components/Destination/Content";
+import { Outlet, useLocation } from "react-router-dom";
+
 
 export default function Destination() {
+  const location = useLocation();
   return (
     <main id={styles.destination}>
       <Nav />
-      <h5><span>01</span> Pick Your Destination</h5>
-      <article style={{color: "#fff"}}>
-        <img src={moon}></img>
-        <Subnav />
-      </article>
+      <div className={styles.grid}>
+        <h5>
+          <span>01</span> Pick Your Destination
+        </h5>
+        <article>
+          <img src={moon} className={styles.planet}></img>
+
+          <div className={styles.content}>
+            <Subnav />
+            {location.pathname === "/destination" ? <Content /> : <Outlet />}
+          </div>
+        </article>
+      </div>
     </main>
   );
 }
