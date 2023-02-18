9a4083009cead221aeccc5f528d938e494b4f9a3
Jesse Krensel <jdkrensel@gmail.com>
2023-02-17 23:44:18 -0500

    remove subnav and img

--- src/routes/Destination.jsx
+++ src/routes/Destination.jsx
@@ -2,12 +2,8 @@
 import React from "react";
 import styles from "./Destination.module.css";
 import Nav from "components/Nav";
-import Subnav from "components/Subnav";
-import moon from "assets/images/destination/image-moon.webp";
 import Content from "components/Destination/Content";
 import { Outlet, useLocation } from "react-router-dom";
-
-
 export default function Destination() {
   const location = useLocation();
   return (
@@ -18,12 +14,7 @@ export default function Destination() {
           <span>01</span> Pick Your Destination
         </h5>
         <article>
-          <img src={moon} className={styles.planet}></img>
-
-          <div className={styles.content}>
-            <Subnav />
-            {location.pathname === "/destination" ? <Content /> : <Outlet />}
-          </div>
+          {location.pathname === "/destination" ? <Content /> : <Outlet />}
         </article>
       </div>
     </main>
