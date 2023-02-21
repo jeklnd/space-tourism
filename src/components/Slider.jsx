111a4df65a76ea5355af4f084c7984f5cabc3c4d
Jesse Krensel <jdkrensel@gmail.com>
2023-02-21 01:16:21 -0500

    create slider, add hover effect

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,24 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "components/Slider.module.css";
-
-function Dot() {
-  return (
-    <svg viewBox="0 0 15 15">
-      <circle cx="7.5" cy="7.5" r="7.5" fill="#fff" />
-    </svg>
-  );
-}
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+import { people } from "data.js";
+import { useLocation } from "react-router-dom";
 
 export default function Slider() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/crew/shuttleworth") {
+      setActiveIndex(1);
+    } else if (location.pathname === "/crew/glover") {
+      setActiveIndex(2);
+    } else if (location.pathname === "/crew/ansari") {
+      setActiveIndex(3);
+    } else {
+      setActiveIndex(0);
+    }
+  });
+
   return (
     <>
-      <ul>
-        {[...Array(4)].map((dot, index) => (
-          <li key={index} className={styles.dot}>
-            <Dot className={styles.svg} />
-          </li>
+      <ul className={styles.ul}>
+        {people.map((person, index) => (
+          <Li
+            key={person.id}
+            className={styles.ul}
+            isActive={activeIndex === index}
+            onClick={() => setActiveIndex(index)}
+            name={person.last.toLowerCase()}
+          ></Li>
         ))}
       </ul>
     </>
   );
 }
+
+function Li({ isActive, onClick, name }) {
+  return (
+    <li className={styles.li} onClick={onClick}>
+      <NavLink className={styles.nav_link} to={`/crew/${name}`}>
+        <svg viewBox="0 0 15 15">
+          <circle
+            className={isActive ? styles.active : styles.circle}
+            cx="7.5"
+            cy="7.5"
+            r="7.5"
+            fill="#ffffff"
+          />
+        </svg>
+      </NavLink>
+    </li>
+  );
+}
+
+Li.propTypes = {
+  isActive: PropTypes.bool,
+  onClick: PropTypes.func,
+  name: PropTypes.string,
+};
