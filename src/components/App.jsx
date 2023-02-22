16ef321a63f2e5e91814426f181c166fc6f37179
Jesse Krensel <jdkrensel@gmail.com>
2023-02-22 14:47:05 -0500

    migrate to typescript

--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
-import Home from "routes/Home";
-
-export default function App() {
-  const location = useLocation();
-  // console.log(location);
-  return <>{location.pathname === "/" ? <Home /> : <Outlet />}</>;
-}
--- src/components/Home/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from "./Button.module.css";
-
-export default function Button() {
-  return (
-    <div className={styles.button}><h4>EXPLORE</h4></div>
-  )
-}
