5310d819829ed3b78fdbd8a07409ba824698453f
Jesse Krensel <jdkrensel@gmail.com>
2023-04-01 12:48:50 -0400

    feat: add drawer component

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,27 +1,65 @@
-// import React from "react";
-// import styles from "components/Drawer.module.css";
-// import { useState } from "react";
-// import PropTypes from "prop-types";
+import React from "react";
+import styles from "components/Drawer.module.css";
+import { NavLink } from "react-router-dom";
+import { useState, useMemo } from "react";
+import { useLocation } from "react-router-dom";
+import { pages } from "data.js";
 
-// export function toggleDrawer() {
-//   const [isOpen, setIsOpen] = useState(false);
-//   if (isOpen === false) {
-//     setIsOpen(true);
-//     console.log("toggled drawer")
-//   } else {
-//     setIsOpen(false);
-//     console.log("toggled drawer")
-//   }
+// interface Props {
+//     isOpen?: boolean;
 // }
 
-// export function Drawer({ isOpen }) {
-//   if (isOpen) {
-//     return <div className={styles.drawer}>Drawer</div>;
-//   }
-// }
+export default function Drawer() {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-// Drawer.propTypes = {
-//   isOpen: PropTypes.bool,
-// };
+  const location = useLocation();
+  useMemo(() => {
+    if (location.pathname.includes("/destination")) {
+      setActiveIndex(1);
+    } else if (location.pathname.includes("/crew")) {
+      setActiveIndex(2);
+    } else if (location.pathname.includes("/technology")) {
+      setActiveIndex(3);
+    } else {
+      setActiveIndex(0);
+    }
+  }, [location.pathname]);
 
-export{}
+  const listItems = pages.map((page, index) => {
+    return (
+      <Li
+        key={page.id}
+        isActive={activeIndex === index}
+        className={styles.li}
+        href={page.href}
+        number={page.number}
+        name={page.name}
+      ></Li>
+    );
+  });
+
+  return (
+    <div className={styles.drawer}>
+      <ul className={styles.ul}>{listItems}</ul>
+    </div>
+  );
+}
+
+interface Props {
+  isActive: boolean;
+  href: string;
+  number: string;
+  name: string;
+  className?: string;
+}
+
+function Li({ isActive, href, number, name }: Props) {
+  return (
+    <li className={styles.li}>
+      <NavLink to={href} className={isActive ? `${styles.active}` : undefined}>
+        <span>{number}</span>
+        {name}
+      </NavLink>
+    </li>
+  );
+}
