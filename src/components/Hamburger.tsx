bdcec52818056890512c0b4f7e5ec3b2c164ae9a
Jesse Krensel <jdkrensel@gmail.com>
2023-04-03 22:12:00 -0400

    refactor: remove fragment

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -4,14 +4,12 @@ import hamburger from "assets/images/shared/icon-hamburger.svg";
 
 export default function Hamburger({ onClick }: Props) {
   return (
-    <>
-      <img
-        className={styles.hamburger}
-        src={hamburger}
-        alt="hamburger"
-        onClick={onClick}
-      />
-    </>
+    <img
+      className={styles.hamburger}
+      src={hamburger}
+      alt="hamburger"
+      onClick={onClick}
+    />
   );
 }
 
