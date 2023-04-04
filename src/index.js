ecbe9aa4b1470ffe3672ac0780e13e6869db59d7
Jesse Krensel <jdkrensel@gmail.com>
2023-04-04 15:13:50 -0400

    perf: disable React strict mode in production

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-      <RouterProvider router={router} />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <RouterProvider router={router} />
+  // </React.StrictMode>
 );
